1600d9720d2e026f4754abc9ce26739552012c40
gascheckking <warpai2k@gmail.com>
2025-05-12 21:20:23 +0200

    Add files via upload

---
+++ api/frame-image.js
@@ -0,0 +1,1 @@
+export default function handler(req, res) { res.setHeader('Content-Type', 'image/png'); res.status(200).send(); }
---
+++ contracts/WarpXP.sol
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract WarpXP { mapping(address => uint) public xp; }
